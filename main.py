ab8ec19f538917e55b72e3c39273d6019baa74cf
aastham-125 <aastha.mahindra125@gmail.com>
2021-06-06 04:48:16 +0530

    Update main.py
    Defining functionalities of different buttons.

--- main.py
+++ main.py
@@ -12,12 +12,29 @@ io_frame.pack(side = TOP)
 text = StringVar()
 
 #frame for input/output result
-io_field = Entry(io_frame, font = ('Arial', 18, 'bold'), textvariable = text, width = 50, bg = "grey", bd = 0, justify = RIGHT)
+io_field = Entry(io_frame, textvariable = text, width = 52, bg = "grey", bd = 0)
 io_field.pack(ipady = 30)
 
 #frame for buttons
 btn_frame = Frame(calc, width = 312, height = 273, bg = "black")
 btn_frame.pack()
+
+#defining buttons
+def click(item):
+    global expression
+    expression = expression + str(item)
+    text.set(expression)
+
+def clear():
+    global expression
+    expression = ""
+    text.set(expression)
+
+def equal(item):
+    global expression
+    result = str(eval(expression))
+    text.set(result)
+
 
 #run calculator
 calc.mainloop()
